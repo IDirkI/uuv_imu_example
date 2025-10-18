e6f05345e4d860c0e30113442c6dc1e82a4e93c4
Berk <berkyilmaz2005@gmail.com>
2025-10-17 21:49:12 -0400

    .gitignore

--- build/uuv_imu_driver_cpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/utux/code/sensor_test/src/uuv_imu_driver_cpp
-# Build directory: /home/utux/code/sensor_test/build/uuv_imu_driver_cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
